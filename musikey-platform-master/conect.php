2d92496ea511e5d598a9a1dfad94002ad7f67e49
Guilherme Silva <44985027+silva-guilherme@users.noreply.github.com>
2019-10-25 21:26:41 -0300

    Delete conect.php

--- musikey-platform-master/conect.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-$host = "localhost"; //nome do site ou numero ip
-$user = "root"; //nome do usuário
-$pwd = ""; //senha se houver
-$db = "musikey"; //nome do banco de dados
-$link = mysqli_connect($host, $user, $pwd, $db); //função de conexão
-
-?>
